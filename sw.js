96a6ac91762f21e560bf4f007117bae011f37349
taesook78-10 <taesook78@gmail.com>
2025-09-12 14:12:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = 'd3l-v6';
-const ASSETS = [
-  '/dailyenglish/',
-  '/dailyenglish/index.html',
-  '/dailyenglish/manifest.webmanifest?v=6',
-  '/dailyenglish/icon-180.png',
-  '/dailyenglish/icon-192.png',
-  '/dailyenglish/icon-512.png',
-  '/dailyenglish/icon-192-maskable.png',
-  '/dailyenglish/icon-512-maskable.png'
-];
-// install/activate/fetch 로직은 이전과 동일
+ const CACHE = 'd3l-v7';
+   const ASSETS = [
+     '/dailyenglish/',
+     '/dailyenglish/index.html',
+     '/dailyenglish/manifest.webmanifest?v=7',
+     '/dailyenglish/icon-180.png',
+     '/dailyenglish/icon-192.png',
+     '/dailyenglish/icon-512.png',
+     '/dailyenglish/icon-192-maskable.png',
+     '/dailyenglish/icon-512-maskable.png'
+   ];
+   // install/activate/fetch 로직은 기존과 동일
 -->
