c28333290552cb6f3cf51195c0ac82e23ccbe36d
taesook78-10 <taesook78@gmail.com>
2025-09-12 13:40:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE = 'd3l-v4'; // <-- 캐시버전 업데이트로 모바일 캐시 강제 갱신
-const ASSETS = ['./','./index.html','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request).then(r=>{
-      const copy = r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request, copy));
-      return r;
-    }).catch(()=>caches.match('./index.html')))
-  );
-});
-
-
+const CACHE = 'd3l-v6';
+const ASSETS = [
+  '/dailyenglish/',
+  '/dailyenglish/index.html',
+  '/dailyenglish/manifest.webmanifest?v=6',
+  '/dailyenglish/icon-180.png',
+  '/dailyenglish/icon-192.png',
+  '/dailyenglish/icon-512.png',
+  '/dailyenglish/icon-192-maskable.png',
+  '/dailyenglish/icon-512-maskable.png'
+];
+// install/activate/fetch 로직은 이전과 동일
+-->
